01f1d6a697309167716dcc8a90f9bcdb291ed6ce
Janson-Gan <janson.gbc@gmail.comgit config --global user.name>
2026-04-07 15:07:54 +0800

    Initial setup

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import app from "./app";
+import dotenv from "dotenv";
+import logger from "./utils/logger";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  logger.info(`Backend Server started listening at http://localhost:${PORT}`);
+});
