b4f0dd5912371392bbdf053f6397138740e08fdc
Janson-Gan <janson.gbc@gmail.comgit config --global user.name>
2026-04-08 16:30:07 +0800

    feat: Add express, logger, cors middleware

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,17 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application } from "express";
+import cors from "cors";
+import health from "./routes/routes";
+import httpLogger from "./middlewares/http.logger";
 
 const app: Application = express();
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Test 123456");
-});
+// Middleware
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(httpLogger);
+
+// Routes
+app.use("/api/v1", health);
 
 export default app;
