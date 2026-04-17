975a4708721229fff565e73b87bac4fe8e576618
Janson-Gan <janson.gbc@gmail.comgit config --global user.name>
2026-04-17 10:53:43 +0800

    feat: Add Postgres database connection

---
+++ src/db/sequelize.ts
@@ -0,0 +1,23 @@
+import { Sequelize } from '@sequelize/core';
+import {PostgresDialect} from '@sequelize/postgres'
+import logger from '../utils/logger';
+import {Student} from '../models';
+
+const sequelize = new Sequelize({
+  dialect: PostgresDialect,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  models: [Student],
+});
+
+export const dbConnection = async () => {
+  try {
+    await sequelize.authenticate();
+    logger.info('Connection to DB successfully.');
+  } catch (error) {
+    logger.error(`Unable to connect to DB: ${error}`);
+  }
+};
