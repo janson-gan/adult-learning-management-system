50ea423085ff94c60b867cc4ad434e014bf93fe4
Janson-Gan <janson.gbc@gmail.comgit config --global user.name>
2026-04-22 08:55:09 +0800

    chore: Add comments on rate limiter

--- src/middlewares/rateLimit.middleware.ts
+++ src/middlewares/rateLimit.middleware.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from "express";
 import rateLimit from "express-rate-limit";
 
+// Wait for 3 minutes after 5 tries of api calling
+// Adjustable as per requirements
 export const rateLimiter = rateLimit({
   windowMs: 5 * 60 * 1000,
   max: 5,
