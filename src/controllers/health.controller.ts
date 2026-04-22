272bd3ac379ef48763cf9a3136326834fdf810fe
Janson-Gan <janson.gbc@gmail.comgit config --global user.name>
2026-04-22 14:45:10 +0800

    chore: Replace double quotes to single quote

--- src/controllers/health.controller.ts
+++ src/controllers/health.controller.ts
@@ -1,13 +1,13 @@
-import { Request, Response } from "express";
-import asyncHandler from "../utils/asyncHandler";
-import config from "../config/config";
+import { Request, Response } from 'express';
+import asyncHandler from '../utils/asyncHandler';
+import config from '../config/app.config';
 
 // Health check response
 const healthCheck = asyncHandler((req: Request, res: Response) => {
   return res.status(200).json({
     success: true,
-    message: "Health ok",
-    enviroment: config.ENV
+    message: 'Health ok',
+    enviroment: config.ENV,
   });
 });
 
