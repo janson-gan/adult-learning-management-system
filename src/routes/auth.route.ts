ed15a4ec1b7255cf995de9b54153584f0d079e97
Janson-Gan <janson.gbc@gmail.comgit config --global user.name>
2026-04-24 15:11:09 +0800

    feat: Add login and get current user route

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,14 +1,25 @@
-import { Router } from "express";
-import {register} from "../controllers/auth.controller";
-import validate from "../middlewares/validate.middleware";
-import { registerSchema } from "../validations/validation";
+import { Router } from 'express';
+import { getMe, login, register } from '../controllers/auth.controller';
+import validate from '../middlewares/validate.middleware';
+import { loginSchema, registerSchema } from '../validations/validation';
+import authenticate from '../middlewares/auth.middleware';
+import authorize from '../middlewares/authorize.middleware';
 
-
-const router = Router()
+const router = Router();
 
 // @route  POST /api/v1/auth/register
 // @desc   Register a new user
 // @access Public
-router.route('/register').post(validate(registerSchema), register)
+router.route('/register').post(validate(registerSchema), register);
 
-export default router
+// @route  POST /api/v1/auth/login
+// @desc   Login user and return token
+// @access Public
+router.route('/login').post(validate(loginSchema), login);
+
+// @route  POST /api/v1/auth/me
+// @desc   Get current logged in user
+// @access Private
+router.route('/me').get(authenticate, getMe)
+
+export default router;
